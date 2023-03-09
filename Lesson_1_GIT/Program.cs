3a58b792485943e9c3bcd208339c6ab35013af03
maxkrup98 <tochnonemaks@gmail.com>
2023-03-09 19:08:38 +0200

    Create console app

---
+++ Lesson_1_GIT/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson_1_GIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Sum = 0; 
+            for (int i = 0; i <=100; i++)
+            {
+                Sum+= i;
+            }
+            Console.WriteLine(Sum);
+        }
+    }
+}
